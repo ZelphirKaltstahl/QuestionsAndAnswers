ab33da4da7eadbd98f68ddaf8daf7cef72e3032a
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-03-18 13:53:13 +0100

    tests for file reader written and tags for tests defined in pytest.ini

--- test/test_FileReader.py
+++ test/test_FileReader.py
@@ -7,6 +7,7 @@ import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../src/')
 
+from FileReader import FileReader
 
 __author__ = '杨小龙'
 
@@ -19,6 +20,10 @@ test_training_state_name = 'test'
 class TestFileReader:
 	"""test class for the Application class"""
 	
+	def __init__(self):
+		self.file_reader = None
+		self.file_path = None
+
 	def setup(self):
 		pass
 
@@ -34,21 +39,61 @@ class TestFileReader:
 		pass
 
 	def setup_method(self, method):
-		pass
+		self.file_reader = FileReader()
+		self.file_path = os.path.join('data', 'testfile.txt')
 
 	def teardown_method(self, method):
-		# Application.input = input
-		# try:
-		# 	os.remove(file_path_to_test_q_and_a)
-		# 	os.remove(file_path_to_test_training_state)
-		# except OSError as e:
-		# 	pass
-		pass
+		try:
+			os.remove(self.file_path)
+		except OSError as e:
+			pass
 
 	@pytest.mark.unit
 	def test_read_file(self):
-		pass
+		lines = [
+			'first line',
+			'second line',
+			'third line',
+			'fourth line',
+			'fifth line',
+			'sixth line'
+		]
+		with open(self.file_path, 'w') as output_file:
+			for line in lines:
+				output_file.write(line + '\n')
+
+		read_lines = self.file_reader.read_file(self.file_path)
+
+		for lineno, line in enumerate(lines):
+			assert line == read_lines[lineno], 'Not all read lines match.'
+
+		for lineno, line in enumerate(read_lines):
+			assert line == lines[lineno], 'Not all read lines match.'
 
 	@pytest.mark.unit
 	def test_read_json(self):
-		pass
+		json_content = {}
+		json_content['identifier'] = 'HSK1'
+		json_content['questions'] = []
+		json_content['questions'].append({
+			"answer": "我",
+			"info": "wǒ",
+			"question": "ich"
+		})
+		json_content['questions'].append({
+			"answer": "的",
+			"info": "de",
+			"question": "von, (besitzanzeigendes Partikel)"
+		})
+		json_content['questions'].append({
+			"answer": "你",
+			"info": "nǐ",
+			"question": "du"
+		})
+
+		with open(file_path, 'w') as output_file:
+			json.dump(json_content, output_file, ensure_ascii=False, indent='\t', sort_keys=True)
+
+		with open(file_path, 'r') as input_file:
+			read_json_content = self.file_reader.read_json(self.file_reader)
+			assert json_content == read_json_content, 'JSON content was not loaded correctly.'
